637d8eab2cd702b664b9f0962d06377979f41577
Bohee Koo <rnqhgml12@naver.com>
2023-03-01 01:50:58 +0900

    react create app으로 todo list 일정 추가 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+//일정 추가하는 기능만 구현
+
+import { useState } from "react";
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault(); //얘가 없으면 버튼 누를시 새로고침됨.
+    if (toDo ===""){
+      return; //공백이 아닐때 투두를 추가하기
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]); //currentarray가 위의 스테이트에서 받아온 todos라고 보면 될듯.
+    setToDo("");
+  };
+  console.log(toDos);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+        onChange = {onChange} 
+        value = {toDo} 
+        type="text" 
+        placeholder="Write your to do..." 
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+        <ul>
+          {toDos.map((item,index) => (
+            <li key={index}>{item}</li> //props를 넣으라는 에러가 떠서 넣음.
+          ))}
+        </ul>
     </div>
   );
 }
